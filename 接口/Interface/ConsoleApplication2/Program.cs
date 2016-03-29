75fe5b62301f60ff4030a3a51e01d36044a3cc67
hc2014 <294836881@qq.com>
2016-03-29 11:03:35 +0800

    interface

---
+++ 接口/Interface/ConsoleApplication2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            People p = new People();           
+            p.say();
+            p.run();
+        }
+    }
+}
