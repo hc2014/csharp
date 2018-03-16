64d49def9791cc128348568364eff4b42ade97e3
294836881@qq.com <294836881@qq.com>
2018-03-16 14:28:32 +0800

    IOC
    创建IOC项目

---
+++ IOC/DAL/Class1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+
+    /// <summary>
+    /// 发货中心
+    /// </summary>
+    public class GoodsOperation
+    {
+        IExpress _express;
+        ILogistic _logistic;
+        public  GoodsOperation(IExpress exp, ILogistic logis)
+        {
+            _express = exp;
+            _logistic = logis;
+        }
+
+        /// <summary>
+        /// 发货
+        /// </summary>
+        public  void SendGoods()
+        {
+            _express.SendGoods(_logistic);
+        }
+    }
+
+
+    public interface IExpress
+    {
+        void SendGoods(ILogistic logistic);
+    }
+
+    public interface ILogistic
+    {
+        string Name { get; }
+    }
+
+
+    public class SF : IExpress
+    {
+        public void SendGoods(ILogistic logistic)
+        {
+            Console.WriteLine(logistic.Name);
+        }
+    }
+
+    public class ByAir : ILogistic
+    {
+        public string Name
+        {
+            get
+            {
+                return "空运";
+            }
+        }
+    }
+
+    public class ByCar : ILogistic
+    {
+        public string Name { get { return "陆运"; } }
+    }
+}
---
+++ IOC/IOC/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Configuration;
+using System.Reflection;
+using DAL;
+using Unity;
+
+namespace IOC
+{
+    public  class Program
+    {
+        static UnityContainer container = new UnityContainer();
+        static void Main(string[] args)
+        {
+            //普通调用方式
+            //GoodsOperation opera = new GoodsOperation(new SF(),new ByAir());
+            //opera.SendGoods();
+
+
+            //通过unity(微软的IOC库)
+            container.RegisterType<IExpress, SF>();
+            container.RegisterType<ILogistic, ByAir>();
+            
+            GoodsOperation op1 = container.Resolve<GoodsOperation>();
+            op1.SendGoods();
+
+            Console.ReadKey();
+        }
+    }
+
+    #region 一般方法
+
+    public class Express
+    {
+        public void SendGoods(ByCar car)
+        {
+            Console.WriteLine("这是" + car.Name);
+        }
+    }
+
+    public class ByCar
+    {
+        public string Name { get { return "陆运"; } }
+    }
+    #endregion
+
+
+}
