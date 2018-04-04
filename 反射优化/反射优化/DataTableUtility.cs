a6d120394f97ab601a73aa77a673292d17341970
294836881@qq.com <294836881@qq.com>
2018-04-04 11:26:14 +0800

    反射优化
    还没有应用到实际项目中

---
+++ 反射优化/反射优化/DataTableUtility.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Reflection.Emit;
+
+
+
+namespace 反射优化
+{
+    public class DataTableUtility
+    {
+
+        public static IEnumerable<T> Get<T>(DataTable table) where T : new()
+        {
+            if (table == null)
+            {
+                yield break;
+            }
+            if (table.Rows.Count == 0)
+            {
+                yield break;
+            }
+            foreach (DataRow row in table.Rows)
+            {
+                yield return Get<T>(row);
+            }
+        }
+
+        public static T Get<T>(DataRow row) where T : new()
+        {
+            return GenericCache<T>.Factory(row);
+        }
+
+        public class GenericCache<T> where T : new()
+        {
+            static GenericCache()
+            {
+                Factory = GetFactory();
+            }
+            public static readonly Func<DataRow, T> Factory;
+
+            private static Func<DataRow, T> GetFactory()
+            {
+                var type = typeof(T);
+                var rowType = typeof(DataRow);
+                var rowDeclare = Expression.Parameter(rowType, "row");
+                var instanceDeclare = Expression.Parameter(type, "instance");
+                var newExpression = Expression.New(type);
+                var instanceExpression = Expression.Assign(instanceDeclare, newExpression);
+                var nullEqualExpression = Expression.Equal(rowDeclare, Expression.Constant(null));
+                var containsMethod = typeof(DataColumnCollection).GetMethod("Contains");
+                var indexerMethod = rowType.GetMethod("get_Item", BindingFlags.Instance | BindingFlags.Public, null,
+                                                      new[] { typeof(string) },
+                                                      new[] { new ParameterModifier(1) });
+                var setExpressions = new List<Expression>();
+                var properties = type.GetProperties(BindingFlags.Instance | BindingFlags.Public);
+                var columns = Expression.Property(Expression.Property(rowDeclare, "Table"), "Columns");
+                foreach (var propertyInfo in properties)
+                {
+                    if (propertyInfo.CanWrite)
+                    {
+                        var propertyName = Expression.Constant(propertyInfo.Name, typeof(string));
+                        var checkIfContainsColumn =
+                            Expression.Call(columns, containsMethod, propertyName);
+                        var propertyExpression = Expression.Property(instanceDeclare, propertyInfo);
+                        var value = Expression.Call(rowDeclare, indexerMethod, propertyName);
+                        var proertyAssign = Expression.Assign(propertyExpression, Expression.Convert(value, propertyInfo.PropertyType));
+                        setExpressions.Add(Expression.IfThen(checkIfContainsColumn, proertyAssign));
+                    }
+                }
+                var checkIfRowIsNull = Expression.IfThenElse(nullEqualExpression, Expression.Empty(), Expression.Block(setExpressions));
+                var body = Expression.Block(new[] { instanceDeclare }, newExpression, instanceExpression, checkIfRowIsNull, instanceDeclare);
+                return Expression.Lambda<Func<DataRow, T>>(body, rowDeclare).Compile();
+            }
+        }
+
+        public static T GetByReflection<T>(DataRow dr) where T : new()
+        {
+            var t = new T();
+            if (dr != null)
+            {
+                foreach (var p in typeof(T).GetProperties())
+                {
+                    if (!dr.Table.Columns.Contains(p.Name))
+                    {
+                        continue;
+                    }
+                    var obj = dr[p.Name];
+                    var set = p.GetSetMethod();
+                    if (set == null)
+                    {
+                        continue;
+                    }
+                    p.SetValue(t, obj, null);
+                }
+            }
+            return t;
+        }
+    }
+}
---
+++ 反射优化/反射优化/Program.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 反射优化
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("固定赋值{0}", "-".PadRight(30, '-'));
+            test("123");
+            Console.WriteLine("GUID赋值{0}", "-".PadRight(30, '-'));
+            test(Guid.NewGuid().ToString());
+
+
+            //User u = new User();
+            //PropertyInfo p = typeof(User).GetProperty("Age");
+            //if (p.CanWrite)
+            //{
+            //    p.SetValue(u,"26");
+            //}
+
+            //设置属性的值
+            //MethodInfo setAge = p.GetSetMethod(true);
+            //setAge.Invoke(u, new object[] { "26" });
+
+
+            //利用委托获取属性的值
+            //u.Age="26";
+            //Func<User, string> getAge = (Func<User, string>)Delegate.CreateDelegate(typeof(Func<User, string>), p.GetGetMethod());
+            //string age = getAge(u);
+
+
+
+            //用表达式树来创建委托的方式 获取属性值
+            ////lambda的参数u 
+            //var param_u = Expression.Parameter(typeof(User), "u");
+            ////lambda的方法体 u.Age 
+            //var pGetter = Expression.Property(param_u, p);
+            ////编译lambda 
+            //var getAge = Expression.Lambda<Func<User, string>>(pGetter, param_u).Compile();
+            //u.Age = "26";
+            //string age = getAge(u);
+
+
+
+            Console.ReadKey();
+        }
+
+
+        static void test(string val)
+        {
+            int count = 1000000;
+
+            User testObj = new User();
+            PropertyInfo propInfo = typeof(User).GetProperty("ID");
+
+            Console.Write("直接访问花费时间：       ");
+            Stopwatch watch1 = Stopwatch.StartNew();
+
+            for (int i = 0; i < count; i++)
+                testObj.ID = val;
+
+            watch1.Stop();
+            Console.WriteLine(watch1.Elapsed.ToString());
+
+            Action<object, object> setter2 = DynamicMethodFactory.CreatePropertySetter(propInfo);
+            Console.Write("EmitSet花费时间：        ");
+            Stopwatch watch2 = Stopwatch.StartNew();
+
+            for (int i = 0; i < count; i++)
+                setter2(testObj, val);
+
+            watch2.Stop();
+            Console.WriteLine(watch2.Elapsed.ToString());
+
+            Console.Write("纯反射花费时间：　       ");
+            Stopwatch watch3 = Stopwatch.StartNew();
+
+            for (int i = 0; i < count; i++)
+                propInfo.SetValue(testObj, val, null);
+
+            watch3.Stop();
+            Console.WriteLine(watch3.Elapsed.ToString());
+
+
+            Console.Write("利用委托花费时间：　       ");
+            Stopwatch watch4 = Stopwatch.StartNew();
+
+
+            MethodInfo setAge = propInfo.GetSetMethod();
+            //利用委托获取属性的值
+            for (int i = 0; i < count; i++)
+            {
+                setAge.Invoke(testObj, new object[] { val });
+            }
+            watch4.Stop();
+            Console.WriteLine(watch4.Elapsed.ToString());
+        }
+
+
+        //对比序列化
+        static void test2()
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("ID");
+            dt.Columns.Add("Name");
+            dt.Columns.Add("Code");
+            dt.Columns.Add("Age");
+            dt.Columns.Add("Gender");
+            dt.Columns.Add("Phone");
+            dt.Columns.Add("Email");
+            dt.Columns.Add("QQ");
+            dt.Columns.Add("Address");
+            for (int i = 0; i < 1000; i++)
+            {
+                DataRow dr = dt.NewRow();
+                foreach (DataColumn col in dt.Columns)
+                {
+                    dr[col] = col.ColumnName + i;
+                }
+                dt.Rows.Add(dr);
+            }
+
+            Stopwatch watch1 = Stopwatch.StartNew();
+
+            var list = DataTableUtility.Get<User>(dt).ToList();
+            watch1.Stop();
+            Console.WriteLine("用表达式树:{0}", watch1.Elapsed.ToString());
+            List<User> uList = new List<User>();
+
+
+            Stopwatch watch2 = Stopwatch.StartNew();
+            foreach (DataRow dr in dt.Rows)
+            {
+                User u = new User();
+                Type t = u.GetType();
+                var pList = t.GetProperties();
+                var pNameList = pList.Select(r => r.Name).ToList();
+                foreach (DataColumn col in dt.Columns)
+                {
+                    if (pNameList.Contains(col.ColumnName))
+                    {
+                        t.GetProperty(col.ColumnName).SetValue(u, dr[col.ColumnName]);
+                    }
+                }
+                uList.Add(u);
+            }
+            watch2.Stop();
+            Console.WriteLine("用反射:{0}", watch2.Elapsed.ToString());
+
+        }
+
+    }
+
+    public static class DynamicMethodFactory
+    {
+        public static Action<object, object> CreatePropertySetter(PropertyInfo property)
+        {
+            if (property == null)
+                throw new ArgumentNullException("property");
+
+            if (!property.CanWrite)
+                return null;
+
+            MethodInfo setMethod = property.GetSetMethod(true);
+
+            DynamicMethod dm = new DynamicMethod("PropertySetter", null,
+                new Type[] { typeof(object), typeof(object) }, property.DeclaringType, true);
+
+            ILGenerator il = dm.GetILGenerator();
+
+            if (!setMethod.IsStatic)
+            {
+                il.Emit(OpCodes.Ldarg_0);
+            }
+            il.Emit(OpCodes.Ldarg_1);
+
+            EmitCastToReference(il, property.PropertyType);
+            if (!setMethod.IsStatic && !property.DeclaringType.IsValueType)
+            {
+                il.EmitCall(OpCodes.Callvirt, setMethod, null);
+            }
+            else
+                il.EmitCall(OpCodes.Call, setMethod, null);
+
+            il.Emit(OpCodes.Ret);   
+
+            return (Action<object, object>)dm.CreateDelegate(typeof(Action<object, object>));
+        }
+
+        private static void EmitCastToReference(ILGenerator il, Type type)
+        {
+            if (type.IsValueType)
+                il.Emit(OpCodes.Unbox_Any, type);
+            else
+                il.Emit(OpCodes.Castclass, type);
+        }
+    }
+
+    public class User
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+
+        public string Code { get; set; }
+
+        public string Age { get; set; }
+
+        public string Gender { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Address { get; set; }
+
+        public string Email { get; set; }
+
+        public string QQ { get; set; }
+    }
+
+    
+}
