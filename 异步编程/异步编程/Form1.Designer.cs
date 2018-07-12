dec0a1e1fd463b8a4393329d17a10238607f6b64
294836881@qq.com <294836881@qq.com>
2018-07-12 15:44:08 +0800

    测试

--- 异步编程/异步编程/Form1.Designer.cs
+++ 异步编程/异步编程/Form1.Designer.cs
@@ -31,13 +31,16 @@
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(107, 50);
+            this.button1.Location = new System.Drawing.Point(160, 75);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(112, 34);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -45,30 +48,55 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(33, 106);
+            this.textBox1.Location = new System.Drawing.Point(50, 159);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(367, 103);
+            this.textBox1.Size = new System.Drawing.Size(548, 152);
             this.textBox1.TabIndex = 1;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(201, 50);
+            this.button2.Location = new System.Drawing.Point(302, 75);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(112, 34);
             this.button2.TabIndex = 2;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(465, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(133, 45);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "异步回调";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(465, 75);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(133, 45);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "等待测试";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(412, 274);
+            this.ClientSize = new System.Drawing.Size(618, 411);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -81,6 +109,8 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- 异步编程/异步编程/Form1.cs
+++ 异步编程/异步编程/Form1.cs
@@ -48,6 +48,8 @@ namespace 异步编程
             var httpClient = new HttpClient();
             int exampleInt = (await httpClient.GetStringAsync("http://www.baidu.com")).Length;
             textBox1.Text += "Preparing to finish ExampleMethodAsync.\\n";
+            Thread.Sleep(5000);
+
             return exampleInt;
         }
 
@@ -66,10 +68,65 @@ namespace 异步编程
         async Task<string> GetString()
         {
             return  await Task.Run(() => {
-                Thread.Sleep(1000);
+                Thread.Sleep(5000);
                 return "没有阻塞...\r\n";
             });
         }
 
+        private void button3_Click(object sender, EventArgs e)
+        {
+            mydelegate = new MyDelegate(TestMethod);
+            IAsyncResult result = mydelegate.BeginInvoke("Thread Param", TestCallback, null);
+            //判断是否执行完成
+            textBox1.Text = "请稍等";
+            //while (!result.AsyncWaitHandle.WaitOne(1000))
+            //{
+            //    textBox1.Text += ".";
+            //}
+        }
+
+        public delegate string MyDelegate(object data);
+        private MyDelegate mydelegate = null;
+
+        public string TestMethod(object data)
+        {
+            string datastr = data.ToString();
+            System.Threading.Thread.Sleep(5000);
+            return datastr;
+        }
+
+        //异步回调函数，异步执行完成以后需要返回的数据
+        public void TestCallback(IAsyncResult data)
+        {
+            //datastr,异步回调最终得到的数据，也就是异步执行程序执行完了以后的返回值
+            //data,异步回调的对象,也是传入回调函数的一个数据,可以用来和返回值进行相关操作
+            string datastr = mydelegate.EndInvoke(data);
+
+            textBox1.BeginInvoke(new MethodInvoker(()=> {
+                textBox1.Text += "\r\n" + datastr;
+            }));
+        }
+
+        private async void button4_Click(object sender, EventArgs e)
+        {
+            //直接 await 会阻塞后面的代码执行
+            string urlContents1 = await Work();
+
+            //赋值给一个Task，然后wait task 是不会阻塞  var a=1的执行
+            Task<string> getStringTask = Work();
+            var a = 1;
+
+            string urlContents = await getStringTask;
+
+            
+        }
+
+        Task<string> Work()
+        {
+            return   Task.Run(() => {
+                Thread.Sleep(5000);
+                return "OK";
+            });
+        }
     }
 }
